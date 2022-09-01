92e281666d125e9329151e9077fec4728051abb7
Nakahara Ryota <zyojozyo@gmail.com>
2022-09-01 12:52:51 +0900

    add loading animation

--- src/pages/works/WorksPage.tsx
+++ src/pages/works/WorksPage.tsx
@@ -1,5 +1,5 @@
 import type { NextPageWithLayout } from 'next'
-import { useContext, useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import styles from './WorksPage.module.scss'
 import { ContactLink } from '@/components/Link/ContactLink/ContactLink'
 import { TagList } from '@/components/Tag/TagList/TagList'
@@ -8,15 +8,25 @@ import { AppContext } from '@/contexts/AppContext'
 
 const WorksPage: NextPageWithLayout = () => {
   const { works, tags } = useContext(AppContext)
+  const [isLoading, setIsLoading] = useState(true)
   const [selectedTagID, setSelectedTagID] = useState('all')
   const handleSelectedID = (selectedTagID: string) => {
-    setSelectedTagID(selectedTagID)
+    setIsLoading(true)
+    setTimeout(() => {
+      setSelectedTagID(selectedTagID)
+    }, 500)
+    setTimeout(() => {
+      setIsLoading(false)
+    }, 1000)
   }
+  useEffect(() => {
+    setIsLoading(false)
+  }, [])
 
   return (
     <>
       <TagList tags={tags} selectedTagID={selectedTagID} handleSelectedID={handleSelectedID} />
-      <WorkList works={works} selectedTagID={selectedTagID} />
+      <WorkList works={works} isLoading={isLoading} selectedTagID={selectedTagID} />
       <ContactLink />
     </>
   )
