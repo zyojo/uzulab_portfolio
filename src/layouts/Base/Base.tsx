1855696b4a9f4ae80416e43cb5758bc2c287a7c8
Nakahara Ryota <zyojozyo@gmail.com>
2022-09-02 17:04:53 +0900

    Update Base.tsx

--- src/layouts/Base/Base.tsx
+++ src/layouts/Base/Base.tsx
@@ -23,6 +23,10 @@ export const BaseLayout = ({ children }: LayoutProps) => {
         <meta property='og:image' content={metaData.ogp.src} />
         <meta property='og:image:width' content={String(metaData.ogp.width)} />
         <meta property='og:image:height' content={String(metaData.ogp.width)} />
+        <meta name='twitter:card' content='summary_large_image' />
+        <meta name='twitter:title' content={metaData.title} />
+        <meta name='twitter:description' content={metaData.description} />
+        <meta property='twitter:image' content={metaData.ogp.src} />
       </Head>
       <main>
         <Header />
