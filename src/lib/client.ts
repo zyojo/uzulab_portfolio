84ac56cfa060e753538965d2ad7c41a47375145f
Nakahara Ryota <zyojozyo@gmail.com>
2022-09-03 14:59:33 +0900

    Update client.ts

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -2,5 +2,5 @@ import { createClient } from 'microcms-js-sdk'
 
 export const client = createClient({
   serviceDomain: 'uzulab',
-  apiKey: String(process.env.X_MICROCMS_API_KEY),
+  apiKey: String(process.env.NEXT_PUBLIC_X_MICROCMS_API_KEY),
 })
