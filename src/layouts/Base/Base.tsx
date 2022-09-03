4c56c40e062651284c7a4c098f4a54fe32a690b8
Nakahara Ryota <zyojozyo@gmail.com>
2022-09-03 17:07:54 +0900

    Update Base.tsx

--- src/layouts/Base/Base.tsx
+++ src/layouts/Base/Base.tsx
@@ -36,7 +36,7 @@ export const BaseLayout = ({ children }: LayoutProps) => {
             __html: `window.dataLayer = window.dataLayer || [];
                   function gtag(){dataLayer.push(arguments);}
                   gtag('js', new Date());
-                  gtag('config', ${process.env.NEXT_PUBLIC_GA_ID});`,
+                  gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');`,
           }}
         />
       </Head>
